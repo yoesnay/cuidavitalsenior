82db8cd6585210398fbadec40db51e5cc97f2948
yoesnay <yoequevedo2013@gmail.com>
2025-10-20 17:56:12 -0600

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,3 @@
+CuidadoVital Senior S.L. (Madrid 2025)
+Archivos públicos para favicon, logo y PWA.
+Color corporativo: #059669
---
+++ service-worker.js
@@ -0,0 +1,3 @@
+self.addEventListener("install", () => {
+  console.log("💚 CuidadoVital Senior — Service Worker instalado.");
+});
